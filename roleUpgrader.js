db5737eec34bc04e35138e9a1d87de9606e95835
Unknown <jessewolf42892@gmail.com>
2018-06-22 21:57:14 -0700

    Tweaking upgraders

--- roleUpgrader.js
+++ roleUpgrader.js
@@ -10,17 +10,17 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.carry.energy == 0) {
+        if(creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[1]);
             }
         }
-        else {
+        // else {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
             }
-        }
+        // }
 	}
 };
 

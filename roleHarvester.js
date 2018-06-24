e6f62384c06b278b764536506e87e24d8c37de30
Unknown <jessewolf42892@gmail.com>
2018-06-23 18:25:09 -0700

    Updated to use working flag for actions

--- roleHarvester.js
+++ roleHarvester.js
@@ -10,14 +10,15 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-        }
-        if(creep.carry.energy == creep.carryCapacity) {   
-            var targets = creep.room.find(FIND_STRUCTURES, {
+      if(creep.memory.working == true && creep.carry.energy == 0) {
+        creep.memory.working = false;
+      }
+      else if(creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+        creep.memory.working = true;
+      }
+
+      if(creep.memory.working == true) {
+          var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
                             structure.energy < structure.energyCapacity;
@@ -29,10 +30,40 @@ var roleHarvester = {
                 }
             }
         }
+        // if(creep.transfer(Game.spawns.Home, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        //   creep.moveTo(Game.spawns.Home)};
+    //   }
+      else{
+        var source = creep.pos.findClosestByPath(FIND_SOURCES);
+        if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(source);
+        }
+      }
+    }
+};
+	   // if(creep.carry.energy < creep.carryCapacity) {
+    //         var sources = creep.room.find(FIND_SOURCES);
+    //         if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+    //             creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+    //         }
+    //     }
+    //     if(creep.carry.energy == creep.carryCapacity) {
+    //         var targets = creep.room.find(FIND_STRUCTURES, {
+    //                 filter: (structure) => {
+    //                     return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+    //                         structure.energy < structure.energyCapacity;
+    //                 }
+    //         });
+    //         if(targets.length > 0) {
+    //             if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+    //                 creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+    //             }
+    //         }
+    //     }
             // if(creep.transfer(Game.spawns['Home'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
             //     creep.moveTo(Game.spawns['Home']);
             // }
-    }
-};
+//     }
+// };
 
 module.exports = roleHarvester;
--- roleUpgrader.js
+++ roleUpgrader.js
@@ -10,18 +10,40 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1]);
-            }
+      if(creep.memory.working == true && creep.carry.energy == 0) {
+        creep.memory.working = false;
+      }
+      else if(creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+        creep.memory.working = true;
+      }
+
+      if(creep.memory.working == true) {
+        if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(creep.room.controller);
         }
-        // else {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
-            }
-        // }
-	}
+      }
+        else{
+          var source = creep.pos.findClosestByPath(FIND_SOURCES);
+          if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(source);
+          }
+        }
+
+  }
 };
+//         if(creep.carry.energy == 0) {
+//             var sources = creep.room.find(FIND_SOURCES);
+//             if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+//                 creep.moveTo(sources[1]);
+//             }
+//         }
+
+//         // else {
+//             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+//                 creep.moveTo(creep.room.controller);
+//             }
+//         // }
+// 	}
+// };
 
 module.exports = roleUpgrader;

84023215c2983b5944b18c4e9736ea4a76e9e65a
Jesse Wolf <jessewolf42892@gmail.com>
2023-01-15 20:39:08 -0800

    Restarted from scratch but kept overall structure

--- code/src/creeps/roles/harvester.js
+++ code/src/creeps/roles/harvester.js
@@ -1,64 +1,21 @@
 var roleHarvester = {
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        let currentCapacity = Game.spawns['spawnW5N8'].room.energyAvailable;
-        let maxCapacity = Game.spawns['spawnW5N8'].room.energyCapacity;
-        if(creep.memory.working == true) {
-            let targets = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
-                    }
-                });
-
-            if(targets) {
-                if(creep.transfer(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) 
-                creep.moveTo(targets, {visualizePathStyle: {stroke: 'ffffff'}});
-            }
-            else {
-                if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(creep.room.controller);
-                }
-            }
-            if(creep.store[RESOURCE_ENERGY] == 0){
-                creep.memory.working = false;
-            }
-        }
-        else if(creep.memory.working == false) {
-
-            /*
-            // Basic bitch setup
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
-            */
-            var containers = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 0
-            });
-            if(containers && creep.withdraw(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(containers[0]);
-            }    
-            else {
-                const target = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES);
-                if(target) {
-                    if(creep.pickup(target) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target);
-                    }
-                }
-            }
-
-            var miners = _.filter(Game.creeps, (creep) => creep.memory.role == 'miner');
-
-            if(containers == null && miners == null) {
-                var sources = creep.room.find(FIND_SOURCES);
-                if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources[0]);
-                }
-            }
-            
-            if(creep.store[RESOURCE_ENERGY] == creep.store.getCapacity()) {
-                creep.memory.working = true;
-            }
-        }
-	}
+  /** @param {Creep} creep **/
+  run: function (creep) {
+    if (creep.store.getFreeCapacity() > 0) {
+      var sources = creep.room.find(FIND_SOURCES);
+      if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0]);
+      }
+    } else if (
+      Game.spawns["W5N8"].energy < Game.spawns["W5N8"].energyCapacity
+    ) {
+      if (
+        creep.transfer(Game.spawns["W5N8"], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE
+      ) {
+        creep.moveTo(Game.spawns["W5N8"]);
+      }
+    }
+  },
 };
+
+module.exports = roleHarvester;

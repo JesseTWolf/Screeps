861c6b00e9cd920c9c445625c1c2c8924d55936a
Jesse Wolf <jessewolf42892@gmail.com>
2023-01-18 20:30:55 -0800

    Reworked to be easier to run per room logic. Some is not used though until I get more spawn code cleaner

--- main.js
+++ main.js
@@ -84,7 +84,8 @@ var roleHarvester = {
         filter: (structure) => {
           return (
             (structure.structureType == STRUCTURE_EXTENSION ||
-              structure.structureType == STRUCTURE_SPAWN) &&
+              structure.structureType == STRUCTURE_SPAWN ||
+              structure.structureType == STRUCTURE_TOWER) &&
             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
           );
         },
@@ -95,6 +96,19 @@ var roleHarvester = {
             visualizePathStyle: { stroke: "#ffffff" },
           });
         }
+      }
+    }
+  },
+};
+
+module.exports = roleHarvester;
+var roleMiner = {
+  /** @param {Creep} creep **/
+  run: function (creep) {
+    if (creep.store.getFreeCapacity() > 0) {
+      var sources = creep.room.find(FIND_SOURCES);
+      if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0], { visualizePathStyle: { stroke: "#ffaa00" } });
       }
     }
   },
@@ -130,13 +144,15 @@ var roleUpgrader = {
 
 module.exports = roleUpgrader;
 const maxNumHarvesters = 2;
-const maxNumUpgraders = 3;
-const maxNumBuilders = 2;
+const maxNumUpgraders = 2;
+const maxNumBuilders = 1;
 const maxNumMiners = 2;
 const maxNumBoxKickers = 3;
 const maxNumDefenders = 2;
 
 const controlledRooms = ["W5N8"];
+const minerCampingSpots = [["14,14", "15,13"]];
+const delphesRooms = ["W3N7"];
 
 // Creep sizing
 let baby = [WORK, CARRY, MOVE, MOVE];
@@ -192,73 +208,110 @@ function printTheThings(room) {
   });
 
   // Print out the total amount of energy available within spawn and all extensions.
-  var energyAvailable = room.energyAvailable;
+  const energyAvailable = room.energyAvailable;
   room.visual.text("Total Energy: " + energyAvailable, 46.2, 9, {
     color: "green",
     font: 0.8,
   });
 
-  var harvesters = _.filter(
+  const creepsInRoom = _.filter(
+    Game.creeps,
+    (creep) => creep.room.name == room.name
+  );
+  room.visual.text("Total Creeps: " + creepsInRoom.length, 46, 0.5, {
+    color: "green",
+    font: 0.8,
+  });
+
+  const harvesters = _.filter(
     Game.creeps,
     (creep) => creep.memory.role == "harvester"
   );
-  room.visual.text("Harvesters: " + harvesters.length, 46, 0.5, {
+  room.visual.text("Harvesters:   " + harvesters.length, 46, 2, {
     color: "green",
     font: 0.8,
   });
 
-  var upgraders = _.filter(
+  const upgraders = _.filter(
     Game.creeps,
     (creep) => creep.memory.role == "upgrader"
   );
-  room.visual.text("Upgraders:  " + upgraders.length, 46, 1.5, {
+  room.visual.text("Upgraders:   " + upgraders.length, 46, 3, {
     color: "green",
     font: 0.8,
   });
 
-  var builders = _.filter(
+  const builders = _.filter(
     Game.creeps,
     (creep) => creep.memory.role == "builder"
   );
-  room.visual.text("Builders:      " + builders.length, 46, 2.5, {
+  room.visual.text("Builders:       " + builders.length, 46, 4, {
     color: "green",
     font: 0.8,
   });
 }
 
-// function () {
+function spawnLogic(room, role) {
+  const energyAvailable = room.energyAvailable;
 
-// }
+  const newName = role + Game.time;
 
-module.exports.loop = function () {
-  for (var name in Memory.creeps) {
-    if (!Game.creeps[name]) {
-      delete Memory.creeps[name];
-      console.log("Clearing non-existing creep memory:", name);
-    }
+  let body;
+  if (energyAvailable >= 300) {
+    body = baby;
   }
+  if (energyAvailable >= 500) {
+    body = level2;
+  }
+  // if (energyAvailable >= 750) {
+  //   body = level3;
+  // }
+  // if (energyAvailable >= 1250) {
+  //   body = monster;
+  // }
+  // if (role == "miner") {
+  //   body = basicMiner;
+  // }
 
-  // For each room in the list of controlled rooms do...
-  controlledRooms.forEach(function (name) {
-    printTheThings(Game.rooms[name]);
+  Game.spawns[room.name].spawnCreep(body, newName, {
+    memory: { role: role },
+  });
+}
+
+function roomLoop(room) {
+  // Print out the total amount of energy available within spawn and all extensions.
+  const energyAvailable = room.energyAvailable;
+  room.visual.text("Total Energy: " + energyAvailable, 46.2, 9, {
+    color: "green",
+    font: 0.8,
   });
 
-  var harvesters = _.filter(
+  const creepsInRoom = _.filter(
     Game.creeps,
-    (creep) => creep.memory.role == "harvester"
-  );
-  var upgraders = _.filter(
-    Game.creeps,
-    (creep) => creep.memory.role == "upgrader"
-  );
-  var builders = _.filter(
-    Game.creeps,
-    (creep) => creep.memory.role == "builder"
+    (creep) => creep.room.name == room.name
   );
 
-  var tower = Game.getObjectById("bf124711ff3f3a01cc17bf28");
+  const totalNumberCreeps = creepsInRoom.length;
+
+  const harvesters = _.filter(
+    Game.creeps,
+    (creep) => creep.memory.role == "harvester" && creep.room.name == room.name
+  );
+
+  const upgraders = _.filter(
+    Game.creeps,
+    (creep) => creep.memory.role == "upgrader" && creep.room.name == room.name
+  );
+
+  const builders = _.filter(
+    Game.creeps,
+    (creep) => creep.memory.role == "builder" && creep.room.name == room.name
+  );
+
+  // TODO: Update this to search within room instead
+  const tower = Game.getObjectById("63c7df8e8537bd003a0cf889");
   if (tower) {
-    var closestDamagedStructure = tower.pos.findClosestByRange(
+    const closestDamagedStructure = tower.pos.findClosestByRange(
       FIND_STRUCTURES,
       {
         filter: (structure) => structure.hits < structure.hitsMax,
@@ -268,7 +321,7 @@ module.exports.loop = function () {
       tower.repair(closestDamagedStructure);
     }
 
-    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    const closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
     if (closestHostile) {
       tower.attack(closestHostile);
     }
@@ -277,35 +330,68 @@ module.exports.loop = function () {
   if (harvesters.length < 2) {
     var newName = "Harvester" + Game.time;
     // console.log("Spawning new harvester: " + newName);
-    Game.spawns["W5N8"].spawnCreep([WORK, CARRY, MOVE], newName, {
+    Game.spawns["W5N8"].spawnCreep([WORK, CARRY, MOVE, MOVE], newName, {
       memory: { role: "harvester" },
     });
   } else if (upgraders.length < 2) {
     var newName = "Upgrader" + Game.time;
     // console.log("Spawning new upgrader: " + newName);
-    Game.spawns["W5N8"].spawnCreep([WORK, CARRY, MOVE], newName, {
+    Game.spawns["W5N8"].spawnCreep([WORK, CARRY, MOVE, MOVE], newName, {
       memory: { role: "upgrader" },
     });
   } else if (builders.length < 1) {
     var newName = "Builder" + Game.time;
     // console.log("Spawning new builder: " + newName);
-    Game.spawns["W5N8"].spawnCreep([WORK, CARRY, MOVE], newName, {
+    Game.spawns["W5N8"].spawnCreep([WORK, CARRY, MOVE, MOVE], newName, {
       memory: { role: "builder" },
     });
   }
 
-  if (Game.spawns["W5N8"].spawning) {
-    var spawningCreep = Game.creeps[Game.spawns["W5N8"].spawning.name];
-    Game.spawns["W5N8"].room.visual.text(
+  // // If we get wiped make 2 harvesters first
+  // if (totalNumberCreeps < 2) {
+  //   spawnLogic(room, "harvester");
+  // } else if (harvesters.length < maxNumHarvesters) {
+  //   spawnLogic(room, "harvester");
+  // }
+  // // else if (minerCampingSpots.length < maxNumMiners) {
+  // // spawnLogic(room, "miner");
+  // // }
+  // // else if (boxKickers.length < maxNumBoxKickers) {
+  // //   // spawnLogic(room.name, "boxKicker");
+  // // }
+  // else if (upgraders.length < maxNumUpgraders) {
+  //   spawnLogic(room, "upgrader");
+  // } else if (builders.length < maxNumBuilders) {
+  //   spawnLogic(room, "builder");
+  // }
+
+  if (Game.spawns[room.name].spawning) {
+    const spawningCreep = Game.creeps[Game.spawns[room.name].spawning.name];
+    Game.spawns[room.name].room.visual.text(
       "🛠️" + spawningCreep.memory.role,
-      Game.spawns["W5N8"].pos.x + 1,
-      Game.spawns["W5N8"].pos.y,
+      Game.spawns[room.name].pos.x + 1,
+      Game.spawns[room.name].pos.y,
       { align: "left", opacity: 0.8 }
     );
   }
+}
 
-  for (var name in Game.creeps) {
-    var creep = Game.creeps[name];
+module.exports.loop = function () {
+  for (const name in Memory.creeps) {
+    if (!Game.creeps[name]) {
+      delete Memory.creeps[name];
+      console.log("Clearing non-existing creep memory:", name);
+    }
+  }
+
+  // For each room in the list of controlled rooms do...
+  controlledRooms.forEach(function (name) {
+    printTheThings(Game.rooms[name]);
+    roomLoop(Game.rooms[name]);
+  });
+
+  for (const name in Game.creeps) {
+    const creep = Game.creeps[name];
     if (creep.memory.role == "harvester") {
       roleHarvester.run(creep);
     }

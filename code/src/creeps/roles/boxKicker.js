fdf3a80276dbfaad48235869a4f29419ec28f130
Jesse Wolf <jessewolf42892@gmail.com>
2023-01-26 18:16:21 -0800

    Updated BoxKicker to only take from Storage if it exists.

--- code/src/creeps/roles/boxKicker.js
+++ code/src/creeps/roles/boxKicker.js
@@ -1,42 +1,43 @@
 var roleBoxKicker = {
   /** @param {Creep} creep **/
   run: function (creep) {
-    // let currentCapacity = creep.room.energyAvailable;
-    // let maxCapacity = creep.room.energyCapacity;
-
     if (creep.memory.working) {
       let targets = creep.pos.findClosestByPath(FIND_STRUCTURES, {
         filter: (structure) => {
           return (
             (structure.structureType == STRUCTURE_EXTENSION ||
-              structure.structureType == STRUCTURE_SPAWN ||
               structure.structureType == STRUCTURE_TOWER) &&
-            // structure.energy < 200)) &&
             structure.energy < structure.energyCapacity
           );
         },
       });
 
       if (targets) {
-        if (creep.transfer(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+        if (creep.transfer(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
           creep.moveTo(targets, { visualizePathStyle: { stroke: "#fffff" } });
-      } else {
-        if (
-          creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE
-        ) {
-          creep.moveTo(creep.room.controller);
         }
       }
       if (creep.store[RESOURCE_ENERGY] == 0) {
         creep.memory.working = false;
       }
     } else if (!creep.memory.working) {
+      var storages = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter: (s) =>
+          s.structureType == STRUCTURE_STORAGE && s.store[RESOURCE_ENERGY] > 0,
+      });
+
       var containers = creep.pos.findClosestByPath(FIND_STRUCTURES, {
         filter: (s) =>
           s.structureType == STRUCTURE_CONTAINER &&
           s.store[RESOURCE_ENERGY] > 0,
       });
+
       if (
+        storages &&
+        creep.withdraw(storages, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE
+      ) {
+        creep.moveTo(storages);
+      } else if (
         containers &&
         creep.withdraw(containers, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE
       ) {

618c43b6af46351a977a4ab1e90bb3a21b35bae9
Jesse Wolf <jessewolf42892@gmail.com>
2023-01-26 18:20:14 -0800

    Got linker working albeit semi hardcoded but that's okay for now

--- code/src/creeps/roles/linker.js
+++ code/src/creeps/roles/linker.js
@@ -1,59 +1,46 @@
-var roleBoxKicker = {
+var roleLinker = {
   /** @param {Creep} creep **/
   run: function (creep) {
     // let currentCapacity = creep.room.energyAvailable;
     // let maxCapacity = creep.room.energyCapacity;
 
-    if (creep.memory.working) {
-      let targets = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-        filter: (structure) => {
-          return (
-            (structure.structureType == STRUCTURE_SPAWN ||
-              structure.structureType == STRUCTURE_TOWER) &&
-            // structure.energy < 200)) &&
-            structure.energy < structure.energyCapacity
-          );
-        },
+    if (!creep.memory.containerID) {
+      /** Commented out until I have second linker up and running
+       *  let occupiedContainers = _.filter(
+        Game.creeps,
+        (creep) => creep.memory.role == "linker"
+      ).map((el) => el.memory.containerID);
+
+      let closestContainer = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter: (structure) =>
+          structure.structureType == STRUCTURE_CONTAINER &&
+          structure.store[RESOURCE_ENERGY] > 0,
       });
 
-      if (targets) {
-        if (creep.transfer(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
-          creep.moveTo(targets, { visualizePathStyle: { stroke: "#fffff" } });
-      } else {
-        if (
-          creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE
-        ) {
-          creep.moveTo(creep.room.controller);
-        }
-      }
-      if (creep.store[RESOURCE_ENERGY] == 0) {
-        creep.memory.working = false;
-      }
-    } else if (!creep.memory.working) {
-      var containers = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-        filter: (s) =>
-          s.structureType == STRUCTURE_CONTAINER &&
-          s.store[RESOURCE_ENERGY] > 0,
-      });
-      if (
-        containers &&
-        creep.withdraw(containers, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE
-      ) {
-        creep.moveTo(containers);
-      } else {
-        const target = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES);
-        if (target) {
-          if (creep.pickup(target) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(target);
-          }
-        }
-      }
+       creep.memory.containerID = closestContainer.id;
+       */
 
-      if (creep.store[RESOURCE_ENERGY] == creep.store.getCapacity()) {
-        creep.memory.working = true;
+      creep.memory.containerID = "63ca254a20730200730248a8";
+    } else {
+      if (creep.store[RESOURCE_ENERGY] < creep.store.getCapacity()) {
+        let container = Game.getObjectById(creep.memory.containerID);
+
+        if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(container, {
+            visualizePathStyle: { stroke: "#ffff00" },
+          });
+        }
+      } else {
+        let link = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+          filter: (structure) => structure.structureType == STRUCTURE_LINK,
+        });
+
+        if (link.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
+          creep.transfer(link, RESOURCE_ENERGY);
+        }
       }
     }
   },
 };
 
-module.exports = roleBoxKicker;
+module.exports = roleLinker;
